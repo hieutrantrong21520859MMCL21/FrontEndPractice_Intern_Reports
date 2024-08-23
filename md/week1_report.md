99a84954bce86e37d59721465f330931d2d7c3ab
DESKTOP-U7NVEFD\LEGION <hieutt03112003@gmail.com>
2024-08-24 04:58:59 +0700

    update

---
+++ md/week1_report.md
@@ -0,0 +1,51 @@
+# BÁO CÁO THỰC TẬP TUẦN 1
+
+_(từ ngày 08/07/2024 đến ngày 14/07/2024)_
+
+## Thông tin chung
+
+- **Cán bộ hướng dẫn thực tập (CBHD)**: Phạm Duy Trung
+
+- **Thời điểm báo cáo**: ngày ***15*** tháng ***07*** năm ***2024***
+
+- **Địa điểm báo cáo**: Số 75 Đường 2 tháng 4, Phường Vĩnh Hòa, TP Nha Trang, Tỉnh Khánh Hòa
+
+## Nội dung công việc
+
+<section>
+    <h3>Kiến thức nền tảng trong lập trình Web</h3>
+    <ul>
+        <li>Khái niệm Internet, lịch sử hình thành và phát triển của Internet</li>
+        <li>Khái niệm WWW, lịch sử hình thành và phát triển của WWW</li>
+    </ul>
+    <h3>Công nghệ nền tảng trong lập trình Web</h3>
+    <ul>
+        <li>Tìm hiểu ngôn ngữ HTML <br> - Tìm hiểu ngôn ngữ CSS</li>
+        <li>Tìm hiểu ngôn ngữ JavaScript</li>
+    </ul>
+    <h3>Các thư viện phổ biến phía Client</h3>
+    <ul>
+        <li>Tìm hiểu thư viện jQuery</li>
+        <li>Tìm hiểu thư viện Lodash</li>
+    </ul>
+    <h3>Tổng quan lập trình mobile</h3>
+    <ul>
+        <li>Giới thiệu về lập trình mobile <br> - Các kiến thức cần thiết về lập trình mobile</li>
+        <li>Các hướng tiếp cận trong lập trình mobile</li>
+        <li>Quy trình trong lập trình mobile</li>
+    </ul>
+    <h3>Làm quen với xây dựng ứng dụng mobile đa nền tảng sử dụng ReactNative</h3>
+    <ul>
+        <li>Khái niệm về ReactNative</li>
+        <li>Ưu, nhược điểm của ReactNative</li>
+        <li>Cấu trúc của ReactNative</li>
+    </ul>
+</section>
+
+<!-- | TT | Nội dung công việc | Chi tiết công việc |
+| :---: | ------------------ |  ----------------- |
+| 1 | _Kiến thức nền tảng trong lập trình Web_ | - Khái niệm Internet, lịch sử hình thành và phát triển của Internet <br> - Khái niệm WWW, lịch sử hình thành và phát triển của WWW |
+| 2 | _Công nghệ nền tảng trong lập trình Web_ | - Tìm hiểu ngôn ngữ HTML <br> - Tìm hiểu ngôn ngữ CSS <br> - Tìm hiểu ngôn ngữ JavaScript |
+| 3 | _Các thư viện phổ biến phía Client_ | - Tìm hiểu thư viện jQuery <br> - Tìm hiểu thư viện Lodash |
+| 4 | _Tổng quan lập trình mobile_ | - Giới thiệu về lập trình mobile <br> - Các kiến thức cần thiết về lập trình mobile <br> - Các hướng tiếp cận trong lập trình mobile <br> - Quy trình trong lập trình mobile |
+| 5 | _Làm quen với xây dựng ứng dụng mobile đa nền tảng sử dụng ReactNative_ | - Khái niệm về ReactNative <br> - Ưu, nhược điểm của ReactNative <br> - Cấu trúc của ReactNative | -->
--- script.js
+++ script.js
@@ -1,17 +1,121 @@
-const reports = document.querySelectorAll("main .reports a[href*='week']");
+// const reports = document.querySelectorAll("main .reports a[href*='week']");
       
-reports.forEach(report => {
+// reports.forEach(report => {
 
-    report.addEventListener('click', () => {
+//     report.addEventListener('click', () => {
 
-        const hashPos = report.href.indexOf('#');
-        const fileName = report.href.slice(hashPos + 1);
+//         const hashPos = report.href.indexOf('#');
+//         const fileName = report.href.slice(hashPos + 1);
 
-        fetch(`./${fileName.replace('-', '')}_report.md`)
-            .then(res => res.text())
-            .then(res => {
+//         fetch(`./${fileName.replace('-', '')}_report.md`)
+//             .then(res => res.text())
+//             .then(res => {
 
-                document.querySelector(`main .sections section[id=${fileName}]`).innerHTML = marked.parse(res);
-            })
+//                 document.querySelector(`main .sections section[id=${fileName}]`).innerHTML = marked.parse(res);
+//             })
+//     })
+// })
+
+const btnOpenMenu = document.getElementById('icon-menu');
+const btnCloseMenu = document.getElementById('icon-menu-close');
+const menu = document.querySelector('main > header dialog');
+
+btnOpenMenu.addEventListener('click', () => {
+
+    menu.showModal();
+})
+
+btnCloseMenu.addEventListener('click', () => {
+
+    menu.close();
+})
+
+const article = document.querySelector('article');
+routie('', () => {
+
+    article.className = 'profile';
+    article.innerHTML =
+    `
+    <div class="avatar">
+
+        <img src="./asset/images/avatar.png" alt="avatar">
+        <p>Trần Trọng Hiếu</p>
+
+    </div>
+
+    <ul>
+
+        <li>
+
+            <img src="./asset/images/icon-location-32.png" alt="icon-location">
+            <p id="address">Phường 25, Quận Bình Thạnh, Thành phố Hồ Chí Minh</p>
+
+        </li>
+
+        <li>
+
+            <img src="./asset/images/icon-email-32.png" alt="icon-email">
+            <p id="email">hieutt03112003@gmail.com</p>
+
+        </li>
+
+        <li>
+
+            <img src="./asset/images/icon-phone-32.png" alt="icon-phone">
+            <p id="phone">0825452467</p>
+
+        </li>
+
+        <li>
+
+            <img src="./asset/images/icon-github-32.png" alt="icon-github">
+               
+            <p>GitHub cá nhân</p>
+
+        </li>
+
+    </ul>
+
+    <footer>
+
+        <p>Liên hệ</p>
+
+        <nav>
+
+            <div>
+       
+                <i class="fa-brands fa-facebook-f"></i>
+         
+            </div>
+         
+            <div>
+         
+                <i class="fa-brands fa-twitter"></i>
+         
+            </div>
+         
+            <div>
+         
+                <i class="fa-brands fa-instagram"></i>
+         
+            </div>
+       
+        </nav>
+
+    </footer>`;
+})
+
+function parseMarkdown(fileName) {
+
+    fetch(`./md/${fileName}.md`).then(res => res.text()).then(text => {
+
+        article.innerHTML = marked.parse(text);
     })
+}
+
+routie('/:fileName', fileName => {
+
+    article.className = `${fileName}`;
+    parseMarkdown(`${fileName.replace('-', '')}_report`);
+    menu.close();
 })
